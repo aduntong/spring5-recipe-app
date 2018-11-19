2ccdf525879c4e4e3899aa53cfed9c39a0a1e0c4
Adun T <aduntong@outlook.com>
2018-11-19 23:43:35 +0800

    http2.0 undertow springboot https

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,16 +1,20 @@
 package guru.springframework.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by jt on 6/1/17.
  */
 @Controller
+@Slf4j
 public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
+    public String getIndexPage( ModelAndView modelAndView ){
+
         return "index";
     }
 }
