1d98ee6748b449cb3eb11cb080f5e125ccfef250
Adun T <aduntong@outlook.com>
2018-11-24 02:34:03 +0800

    commit

--- src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
+++ src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.util.AssertionErrors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,6 +15,6 @@ public class Spring5RecipeAppApplicationTests {
 
 	@Test
 	public void test2(){
-		AssertionErrors.assertTrue( "", true );
+
 	}
 }
