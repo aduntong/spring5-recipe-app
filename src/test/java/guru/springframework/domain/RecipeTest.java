3b87e2b5053386db8d62268bede0e1ef40daf9c2
Adun T <aduntong@outlook.com>
2018-11-23 23:41:25 +0800

    更新 lombok

---
+++ src/test/java/guru/springframework/domain/RecipeTest.java
@@ -0,0 +1,146 @@
+package guru.springframework.domain;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Adun Tong
+ * @date 2018/11/23
+ **/
+public class RecipeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void setNotes() {
+	}
+
+	@Test
+	public void addIngredient() {
+	}
+
+	@Test
+	public void getId() {
+	}
+
+	@Test
+	public void getDescription() {
+	}
+
+	@Test
+	public void getPrepTime() {
+	}
+
+	@Test
+	public void getCookTime() {
+	}
+
+	@Test
+	public void getServings() {
+	}
+
+	@Test
+	public void getSource() {
+	}
+
+	@Test
+	public void getUrl() {
+	}
+
+	@Test
+	public void getDirections() {
+	}
+
+	@Test
+	public void getIngredients() {
+	}
+
+	@Test
+	public void getImage() {
+	}
+
+	@Test
+	public void getDifficulty() {
+	}
+
+	@Test
+	public void getNotes() {
+	}
+
+	@Test
+	public void getCategories() {
+	}
+
+	@Test
+	public void setId() {
+	}
+
+	@Test
+	public void setDescription() {
+	}
+
+	@Test
+	public void setPrepTime() {
+	}
+
+	@Test
+	public void setCookTime() {
+	}
+
+	@Test
+	public void setServings() {
+	}
+
+	@Test
+	public void setSource() {
+	}
+
+	@Test
+	public void setUrl() {
+	}
+
+	@Test
+	public void setDirections() {
+	}
+
+	@Test
+	public void setIngredients() {
+	}
+
+	@Test
+	public void setImage() {
+	}
+
+	@Test
+	public void setDifficulty() {
+	}
+
+	@Test
+	public void setCategories() {
+	}
+
+	@Test
+	public void equals() {
+	}
+
+	@Test
+	public void canEqual() {
+	}
+
+	@Test
+	public int hashCode() {
+		return 0;
+	}
+
+	@Test
+	public String toString() {
+		return "";
+	}
+}
