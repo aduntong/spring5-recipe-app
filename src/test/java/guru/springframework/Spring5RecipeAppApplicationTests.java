782ab010b2a6ce5369f71d2bbe7ef70a5041677d
Adun T <aduntong@outlook.com>
2018-11-24 00:55:51 +0800

    更新 lombok

--- src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
+++ src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.AssertionErrors;
+
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +14,8 @@ public class Spring5RecipeAppApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertTrue(true);
+		AssertionErrors.assertTrue( "", true);
 	}
 
 }
